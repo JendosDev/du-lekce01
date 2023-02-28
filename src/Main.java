4c8d498a346a9e4b17e23b48a05a160e97541c6d
JendosDev1 <vesjan007@gmail.com>
2023-02-28 15:34:41 +0100

    Opravení repozitáře.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import javax.naming.Name;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String nameAndLastname;
+        LocalDate bornDate;
+        int contract;
+        double quantityInTons;
+        String residenceOfEmployee;
+        String SPZ;
+        double vehicleConsumptionInLit;
+        String computerIP;
+
+        nameAndLastname = "Tomáš Novotný";
+        bornDate = LocalDate.of(1987, 10, 13);
+        contract = 8;
+        quantityInTons = 18.6;
+        residenceOfEmployee = "Prague";
+        SPZ = "A23 5213";
+        vehicleConsumptionInLit = 12;
+        computerIP = "IP adresa verze 4";
+
+        System.out.println(quantityInTons / contract);
+
     }
 }
+
